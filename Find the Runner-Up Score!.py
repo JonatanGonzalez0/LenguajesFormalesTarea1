9b2b5c82f1f12dc0edf71a0f6a4f7f6fe60d327a
JonatanGonzalez0 <70916578+JonatanGonzalez0@users.noreply.github.com>
2021-02-04 13:55:13 -0600

    Update Find the Runner-Up Score!.py

--- Find the Runner-Up Score!.py
+++ Find the Runner-Up Score!.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    lis = list(map(int,raw_input().strip().split()))[:n]
+    lis = list(map(int,input().strip().split()))[:n]
     x = max(lis)
     while max(lis) == x:
         lis.remove(max(lis))
